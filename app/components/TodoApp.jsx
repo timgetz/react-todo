95172afaff6e992f3c32b972e7b349dfa5cb9bf5
Tim Getz <timgetz@gmail.com>
2016-10-04 00:57:13 -0400

    Initial Commit

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,38 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function() {
+      return {
+          todos: [
+              {
+                  id: 1,
+                  text: 'Walk the dog'
+              },
+              {
+                  id: 2,
+                  text: 'Clean the yard'
+              },
+              {
+                  id: 3,
+                  text: 'Eat Lunch'
+              },
+              {
+                  id: 4,
+                  text: 'Complete course'
+              }
+          ]
+      }
+    },
     render: function() {
+        var {todos} = this.state;
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     }
-})
+});
+
 module.exports = TodoApp;
