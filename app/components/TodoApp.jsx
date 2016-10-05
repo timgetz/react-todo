c4105992a7bfff3b4e05274d527f0fe63ff340c2
Tim Getz <timgetz@gmail.com>
2016-10-04 22:17:48 -0400

    Add todo and tests.

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-
+var uuid = require('node-uuid');
 var TodoApp = React.createClass({
     getInitialState: function() {
       return {
@@ -10,26 +10,34 @@ var TodoApp = React.createClass({
           searchText: '',
           todos: [
               {
-                  id: 1,
+                  id: uuid(),
                   text: 'Walk the dog'
               },
               {
-                  id: 2,
+                  id: uuid(),
                   text: 'Clean the yard'
               },
               {
-                  id: 3,
+                  id: uuid(),
                   text: 'Eat Lunch'
               },
               {
-                  id: 4,
+                  id: uuid(),
                   text: 'Complete course'
               }
           ]
       }
     },
     handleAddTodo: function(text) {
-        alert('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        })
     },
     handleSearch: function(showCompleted, searchText) {
         this.setState({
