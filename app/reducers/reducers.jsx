243f2da57a637f51a12ab836246520137eb39b91
Tim Getz <timgetz@gmail.com>
2016-10-06 23:39:04 -0400

    Fixed Add Todo with redux and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -43,8 +43,9 @@ export var todosReducer = (state = [], action) => {
                         completed: nextCompleted,
                         completedAt: nextCompleted ? moment().unix() : undefined
                     };
+                } else {
+                    return todo;
                 }
-                return todo;
             });
         default:
             return state;
