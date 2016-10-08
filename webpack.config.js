54618ccb5eb649fab3edfcae83c2e12da15a0841
Tim Getz <timgetz@gmail.com>
2016-10-08 15:55:43 -0400

    Debugging heroku3 - Cannot resolve module 'TodoAPI'

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
       applicationStyles: 'app/styles/app.scss',
       actions: 'app/actions/actions.jsx',
       reducers: 'app/reducers/reducers.jsx',
-      configureStore: 'app/store/configureStore.jsx'
+      configureStore: 'app/store/configureStore.jsx',
+      TodoAPI: 'app/api/TodoAPI.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
