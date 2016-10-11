388632f858e602083477ead65802d8dd7399773f
Tim Getz <timgetz@gmail.com>
2016-10-10 22:36:03 -0400

    Trying firebase authentication

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ heroku config:set API_KEY=AIzaSyDN-XDkGMIKdVHtOeyHEXm2O7hPnsTliTw
 heroku config:set AUTH_DOMAIN=todo-app-c171a.firebaseapp.com
 heroku config:set DATABASE_URL=https://todo-app-c171a.firebaseio.com
 heroku config:set STORAGE_BUCKET=
+
+
+
+api.openweathermap.org/data/2.5/weather?q=London,uk&units=imperial&appid=e7f3ac55dc394facc32258ac456be7d9
+
+webpack --watch --progress
+
+set NODE_ENV=production webpack -p
+
+node server.js
+
+heroku create https://mysterious-tor-31955.herokuapp.com/
+
+git push heroku master
+
+heroku open "# react-boilerplate2" "# react-timer" "# react-timer" "# react-boilerplate3"
+
+bundle.js:22 You are currently using minified code outside of NODE_ENV === 'production'. This means that you are running a slower development build of Redux. You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify or DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) to ensure you have the correct code for your production build.
--- app/app.jsx
+++ app/app.jsx
@@ -3,9 +3,21 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
+import Login from 'Login';
+import TodoApp from 'TodoApp';
+import firebase from 'app/firebase/';
+
+firebase.auth().onAuthStateChanged((user) => {
+    console.log('AuthStateChanged', user);
+    if (user) {
+        hashHistory.push('/todos');
+    } else {
+        hashHistroy.push('/');
+    }
+});
+
 
 store.dispatch(actions.startAddTodos());
 
@@ -17,7 +29,12 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp/>
+    <Router history={hashHistory}>
+        <Route path="/">
+            <Route path="todos" component={TodoApp}/>
+            <IndexRoute components={Login}/>
+        </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
